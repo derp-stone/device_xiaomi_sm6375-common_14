d9e3b07bb6a282ce111d54a48b8448e8a2fabfb0
boedhack99 <boedhack@gmail.com>
2023-07-10 14:26:15 +0700

    sm6375-common: Switch to source build sensors HAL
    Change-Id: Ib0f45b7fc134a1eb71e71c6c54937b40a73625c0

--- holi.mk
+++ holi.mk
@@ -409,7 +409,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.xiaomi-multihal \
+    android.hardware.sensors@2.1-service.multihal \
     libsensorndkbridge
 
 # Servicetracker
